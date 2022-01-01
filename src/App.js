20ff4c6f434ebf4ec3ba223caac3ae228e3731a1
bilalmuhammad14-7-98 <bilalmuhammad266@yahoo.com>
2022-01-01 19:48:01 +0500

    Adding common components and setting up the Login form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import reducers from './reducers';
+import {initializeApp} from 'firebase/app';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAFx-C3Aodyifhz17X-LoYFm0JOVsq-DFM',
+      authDomain: 'manager-99c92.firebaseapp.com',
+      projectId: 'manager-99c92',
+      storageBucket: 'manager-99c92.appspot.com',
+      messagingSenderId: '579839058691',
+      appId: '1:579839058691:web:dfa7904a2aed09b9893f36',
+      measurementId: 'G-9QRWDYRFYD',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    console.log(app);
+  }
+
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(reducers)}>
         <View>
-          <Text>Hello</Text>
+          {/* <Text>Hello World</Text> */}
+          <LoginForm />
         </View>
       </Provider>
     );
